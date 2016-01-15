963c661a7eb7eadc7d283edd9adc8fdf566c73b4
Mayusor <mayuso666@gmail.com>
2016-01-15 10:21:55 +0100

    Added missing algorithm functions to JDMMining. Added KDtreeKNN and som classification algorithms.

--- src/AI/Classificator.java
+++ src/AI/Classificator.java
@@ -5,12 +5,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 
+import net.sf.javaml.classification.KDtreeKNN;
 import net.sf.javaml.classification.Classifier;
 import net.sf.javaml.classification.KNearestNeighbors;
 import net.sf.javaml.classification.MeanFeatureVotingClassifier;
 import net.sf.javaml.classification.NearestMeanClassifier;
 import net.sf.javaml.classification.ZeroR;
+import net.sf.javaml.classification.SOM;
+
 import net.sf.javaml.classification.tree.RandomForest;
+import net.sf.javaml.clustering.SOM.GridType;
+import net.sf.javaml.clustering.SOM.LearningType;
+import net.sf.javaml.clustering.SOM.NeighbourhoodFunction;
 import net.sf.javaml.core.Dataset;
 import net.sf.javaml.core.Instance;
 import net.sf.javaml.tools.data.FileHandler;
@@ -22,6 +28,17 @@ public class Classificator {
 	
 	public Classificator(){}
 
+	
+	public Classifier KDtreeKNN(String path, int key)
+			throws IOException, IllegalArgumentException {
+
+		Classifier model = new  KDtreeKNN(key);
+		Dataset data = FileHandler.loadDataset(new File(path), key, ",");
+		model.buildClassifier(data);
+
+		return model;
+	}
+	
 	public Classifier randomForest(String path, int numberOfTrees, int key)
 			throws IOException, IllegalArgumentException {
 
@@ -53,13 +70,21 @@ public class Classificator {
 		return model;
 	}
 
-	public Classifier MeanFeatureVotingClassifier(String path, int key)
+	public Classifier meanFeatureVotingClassifier(String path, int key)
 			throws IOException, IllegalArgumentException {
 
 		Classifier model = new MeanFeatureVotingClassifier();
 		Dataset data = FileHandler.loadDataset(new File(path), key, ",");
 		model.buildClassifier(data);
 
+		return model;
+	}
+	public Classifier som(String path, int key, int xdim, int ydim,GridType grid, int iterations, double learningRate, int initialRadius, LearningType learning, NeighbourhoodFunction nbf)
+			throws IOException, IllegalArgumentException {
+
+		Classifier model = new SOM(xdim, ydim, grid, iterations,learningRate, initialRadius, learning, nbf);
+		Dataset data = FileHandler.loadDataset(new File(path), key, ",");
+		model.buildClassifier(data);
 		return model;
 	}
 	public Classifier zeroR(String path, int key)
@@ -71,7 +96,6 @@ public class Classificator {
 
 		return model;
 	}
-	
 
 	public ArrayList<Object> clasify(String path, int key, Classifier model)
 			throws IOException {
--- src/JDMMining.java
+++ src/JDMMining.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import AI.Classificator;
 import AI.Cluster;
 import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.clustering.SOM.GridType;
+import net.sf.javaml.clustering.SOM.LearningType;
+import net.sf.javaml.clustering.SOM.NeighbourhoodFunction;
 import net.sf.javaml.core.Dataset;
 
 public class JDMMining {
@@ -14,18 +17,42 @@ public class JDMMining {
 		cluster = new Cluster();
 	}
 
+	public Classifier KDtreeKNN(String path, int key) throws IOException, IllegalArgumentException {
+		return classificator.KDtreeKNN(path, key);
+	}
 	public Classifier randomForest(String path, int numberOfTrees, int key) throws IllegalArgumentException, IOException {
 		return classificator.randomForest(path, numberOfTrees, key);
 	}
 	public Classifier kNearestNeighbors(String path, int neighbors, int key) throws IOException, IllegalArgumentException {
 		return classificator.kNearestNeighbors(path, neighbors, key);
 	}
+	
+	public Classifier nearestMeanClassifier(String path, int key)
+			throws IOException, IllegalArgumentException {
+		return classificator.nearestMeanClassifier(path, key);
+	}
+	public Classifier meanFeatureVotingClassifier(String path, int key)
+			throws IOException, IllegalArgumentException {
+		return classificator.meanFeatureVotingClassifier(path, key);
+	}
+	public Classifier som(String path, int key, int xdim, int ydim,GridType grid, int iterations, double learningRate, int initialRadius, LearningType learning, NeighbourhoodFunction nbf)
+			throws IOException, IllegalArgumentException {
+		return classificator.som(path, key, xdim, ydim, grid, iterations, learningRate, initialRadius, learning, nbf);
+	}
+	public Classifier zeroR(String path, int key)
+			throws IOException, IllegalArgumentException {
+		return classificator.zeroR(path, key);
+	}
+
+	
+	
 	public ArrayList<Object> clasify(String path, int key, Classifier tree) throws IOException{
 		return classificator.clasify(path, key, tree);
 	}
 	public Dataset[] kMeans(String path, int numberOfClusters, int numberOfIterations, int key) throws IOException{
 		return cluster.kMeans(path, numberOfClusters, numberOfIterations, key);
 	}
+	
 	
 	
 }
