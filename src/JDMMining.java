e520abc3d2081f2a646ca17e8c3110ae0f45ebea
Mayusor <mayuso666@gmail.com>
2016-01-15 09:32:32 +0100

    Added used libraries (3 POI .JARs and 1 JavaML .JAR).

--- src/JDMMining.java
+++ src/JDMMining.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
 import java.util.ArrayList;
 
-import net.sf.javaml.classification.Classifier;
-import net.sf.javaml.core.Dataset;
-
 import AI.Classificator;
 import AI.Cluster;
+import net.sf.javaml.classification.Classifier;
+import net.sf.javaml.core.Dataset;
 
 public class JDMMining {
 	Classificator classificator;
